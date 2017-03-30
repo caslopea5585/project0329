5bf192a11284219d339d01593cbdb10e893c897d
user\user1 <caslopea5585@gmail.com>
2017-03-30 10:08:43 +0900

    word 클래스 끝!

--- src/game/word/GamePanel.java
+++ src/game/word/GamePanel.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -30,7 +32,7 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 	JLabel la_score; //게임점수
 	Choice choice; //단어 선택 드랍박스
 	JTextField t_input; //게임 입력창
-	JButton bt_start,bt_pause; //게임시작
+	JButton bt_start,bt_pause,bt_stop; //게임시작
 	
 	FileInputStream fis;
 	InputStreamReader reader; //파일을 기반으로한 문자스트림.
@@ -41,6 +43,7 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 	ArrayList<String> wordList = new ArrayList<String>();
 	Thread thread; //단어게임을 진행할 쓰레드...
 	boolean flag = true;
+	boolean isDown=true;
 	ArrayList<Word> words = new ArrayList<Word>();
 	
 	
@@ -72,6 +75,7 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 		t_input= new JTextField(10);
 		bt_start=new JButton("Start");
 		bt_pause=new JButton("Pause");
+		bt_stop = new JButton("Stop");
 		
 		
 		p_west.setPreferredSize(new Dimension(150, 700));
@@ -81,14 +85,37 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 		choice.setPreferredSize(new Dimension(135, 40));
 		choice.addItemListener(this);
 		
+		//버튼과 리스너 연결
 		bt_start.addActionListener(this);
 		bt_pause.addActionListener(this);
+		bt_stop.addActionListener(this);
+		
+		//텍스트필드와 리스너 연결
+		t_input.addKeyListener(new KeyAdapter() {
+				
+				public void keyReleased(KeyEvent e) {
+					if(e.getKeyCode()==KeyEvent.VK_ENTER){
+						//화면에 존재하는 words와 입력값 비교하여 맞으면 words에서 객체 삭제...
+						String value = t_input.getText();
+						
+						for(int i=0; i<words.size();i++){
+							if(words.get(i).name.equals(value)){
+								words.remove(i);
+								t_input.setText("");
+							}	
+							
+						}
+						
+					}		
+				}
+		});
 		
 		p_west.add(la_user);
 		p_west.add(choice);
 		p_west.add(t_input);
 		p_west.add(bt_start);
 		p_west.add(bt_pause);
+		p_west.add(bt_stop);
 		p_west.add(la_score);
 		
 		setLayout(new BorderLayout());
@@ -135,12 +162,18 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 				//스트림을 버퍼 처리 수준까지 올림
 				buffr = new BufferedReader(reader);
 				String data;
+				//기존 wordList를 비우자..
+				wordList.removeAll(wordList);
+				
+				
 				while(true){
 					data = buffr.readLine();
 					if(data==null)break;
 					wordList.add(data);
 					System.out.println(data);
 				}
+				
+				System.out.println("현재까지 wordLIst는  = "+ wordList.size());
 				//준비된 단어를 화면에 보여주기
 				createWord();
 				
@@ -185,13 +218,32 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 	
 	//게임시작
 	public void startGame(){
+		
 		if(thread==null){
+			flag=true;
 			thread = new Thread(this);
 			thread.start();
 		}
+		
 	}
-	//게임중지
+	//게임중지 or 재시작
 	public void pauseGame(){
+		isDown = !isDown;
+	}
+	//게임종료
+	/*--결국 처음으로 돌아가자!!!!!!!!!!!!
+	 * 1. wordList() 비우기
+	 * 2. words(Word의 인스턴스들이 들어있는 ) 비우기
+	 * 3. choice 초기화 (index =0);
+	 * 4. while문의 flag를 false로...
+	 * 5. thread를 null로 다시 초기화
+	 * */
+	public void stopGame(){
+		wordList.removeAll(wordList);
+		words.removeAll(words);
+		choice.select(0); //첫번째 요소 강제 선택
+		flag = false; //while문 중지목적.
+		thread=null;
 		
 	}
 	
@@ -217,6 +269,8 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 			startGame();
 		}else if(obj == bt_pause){
 			pauseGame();
+		}else if(obj==bt_stop){
+			stopGame();
 		}
 	}
 	public void run() {
@@ -227,12 +281,15 @@ public class GamePanel extends JPanel implements ItemListener,Runnable,ActionLis
 				e.printStackTrace();
 			}
 			//down();
-			//모든 단어들에 대해서 tick()
-			for(int i=0; i<words.size();i++){
-				words.get(i).tick();
+			
+			if(isDown){
+				//모든 단어들에 대해서 tick()
+				for(int i=0; i<words.size();i++){
+					words.get(i).tick();
+				}
+				//모든 단어들에 대해서 repaint()
+				p_center.repaint();
 			}
-			//모든 단어들에 대해서 repaint()
-			p_center.repaint();
 		}
 	}
 	
